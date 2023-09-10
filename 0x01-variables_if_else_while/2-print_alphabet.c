3f1b5c39b96010fdab4eb5e96333e044a5cd2390
mbouna <mohammed.bouna07@gmail.com>
2023-09-10 10:11:41 -0700

    task 1 for c programming

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main
+ * Description: Get a random number and check its last digit, finally compare it with 5
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
