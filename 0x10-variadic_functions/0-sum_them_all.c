cfb351222bf9173b569cc41681a897a8b08df44e
bouna <mohammed.bouna07@gmail.com>
2023-10-12 09:55:43 -0700

    correct first task

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,9 +1,11 @@
 #include "variadic_functions.h"
 /**
  * sum_them_all - returns the sum of all its parameters.
- * @n: amount of the arguments.
+ * @n: The number of parameters passed to the function.
+ * @...: A variable number of parameters to calculate the sum of.
  *
- * Return: sum of its parameters.
+ * Return: If n== 0 -0.
+ *         Otherwise - the sum of all parameters.
  */
 int sum_them_all(const unsigned int n, ...)
 {
@@ -11,10 +13,7 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	int sum = 0;
 
-	if (n == 0)
-		return (0);
-
-	va_start(valist, n);
+      	va_start(valist, n);
 
 	for (i = 0; i < n; i++)
 		sum += va_arg(valist, int);
